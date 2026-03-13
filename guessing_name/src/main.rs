bfcb31725dbc9bf60dc4272b1926751605307c2c
heavenyoung <jacksonou3@gmail.com>
2026-03-13 19:47:12 +0300

    написан корректный код

---
+++ guessing_name/src/main.rs
@@ -0,0 +1,14 @@
+use std::io;
+
+fn main() {
+    println!("Угадайте число!");
+
+    println!("Пожалуйста введите свою догадку.");
+
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess)
+        .expect("Не получилось прочитать строку");
+
+    println!("Вы загадали {}", guess);
+}
