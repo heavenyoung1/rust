4f7d09c62b5afa3696ace0375c5cf94d176dfe61
heavenyoung <jacksonou3@gmail.com>
2026-03-22 17:52:56 +0300

    Обработка ошибок текста

--- guessing_name/src/main.rs
+++ guessing_name/src/main.rs
@@ -14,8 +14,11 @@ fn main() {
         io::stdin().read_line(&mut guess)
             .expect("Не получилось прочитать строку");
 
-        let guess: u32 = guess.trim().parse()
-            .expect("Пожалуйста введите число!");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        
 
         println!("Вы загадали {}", guess);
 
